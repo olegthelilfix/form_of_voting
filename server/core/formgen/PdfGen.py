43c82cb1d1583f94b6af540a6713e5d348737514
Aleksandrov Oleg <smith5989@gmail.com>
2015-12-04 20:47:25 +0300

    ref

---
+++ server/core/formgen/PdfGen.py
@@ -0,0 +1,23 @@
+# -*- coding: utf8 -*-
+__author__ = 'Aleksandrov Oleg, 4231'
+
+from core.formgen.RenderHtml import RenderHtml
+from weasyprint import HTML
+import codecs
+import os
+
+
+class PdfGen:
+    __dir = os.getcwd() + "/result/htmlcode.html"
+    __result_dir = os.getcwd() + "/result/result.pdf"
+
+    def execute(self):
+        render = RenderHtml()
+        value = render.split_question_on_pages()
+        with codecs.open(self.__dir, 'w', 'utf8') as f2:
+            f2.write(value)
+
+        pdf = HTML(self.__dir)
+        pdf.write_pdf(self.__result_dir)
+
+        return self.__result_dir
--- server/core/formgen/main.py
+++ server/core/formgen/main.py
@@ -1,15 +1,7 @@
 # -*- coding: utf8 -*-
 __author__ = 'Aleksandrov Oleg, 4231'
 
-from core.formgen.RenderHtml import RenderHtml
-from weasyprint import HTML
-import codecs
-import os
+from core.formgen.PdfGen import PdfGen
 
-dir = os.getcwd() + "/result/htmlcode"
-render = RenderHtml()
-value = render.split_question_on_pages()
-with codecs.open(dir, 'w', 'utf8') as f2:
-    f2.write(value)
-pdf = HTML(dir)
-pdf.write_pdf(os.getcwd() + "/result/result.pdf")
+pg = PdfGen()
+print(pg.execute())
