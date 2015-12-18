8875bcaa24379b5e5a0e991af15f1545989e082a
Rentert <smith5989@gmail.com>
2015-12-18 22:42:09 +0300

    remove thrash

--- server/core/formgen/FormData.py
+++ server/core/formgen/FormData.py
@@ -9,10 +9,10 @@ class FormData:
     __qs = []
     __small_qr = []
     __version = "0.1"
-    __id_user = "null"
-    __id_owner = "null"
-    __id_premise = "null"
-    __id_meeting = "null"
+    __id_user = "nullnullnull"
+    __id_owner = "nullnullnull"
+    __id_premise = "nullnullnull"
+    __id_meeting = "nullnullnull"
     __fio = "______________________"
 
     def __init__(self, id_user, id_meeting):
@@ -20,7 +20,7 @@ class FormData:
         self.__id_user = str(id_user)
         qs_small_qr = self.__dao.get_question(id_meeting)
         for value in qs_small_qr:
-            self.__small_qr.append(value[0])
+            self.__small_qr.append('s' + str(value[0]))
             self.__qs.append(value[1])
         self.__fio = self.__dao.get_owner_fio(id_user)
 
@@ -42,7 +42,11 @@ class FormData:
 
     # версия| id_user | id_owner| id_premise | id meeting | количество страниц| и номер текущей|
     def get_big_qr_code_date(self):
-        return self.__version + self.__id_user + self.__id_owner + self.__id_premise + self.__id_meeting
+        return 'b' + self.__version.ljust(10, ' ') + '|' \
+               + self.__id_user.ljust(10, ' ') + '|' \
+               + self.__id_owner.ljust(10, ' ') + '|' \
+               + self.__id_premise.ljust(10, ' ') + '|' \
+               + self.__id_meeting.ljust(10, ' ')
 
     def get_questions(self):
         return self.__qs
