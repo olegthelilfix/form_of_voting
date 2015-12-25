901229e17c02c83ca09739f7d80438b9dea16a8c
Rentert <smith5989@gmail.com>
2015-12-25 03:55:29 +0300

    ref

--- server/core/DAO/PDFGenDAOPostgres.py
+++ server/core/DAO/PDFGenDAOPostgres.py
@@ -1,5 +1,6 @@
 import pg8000
 
+
 class PDFGenDAOPostgres:
     _connect = None
 
@@ -19,36 +20,35 @@ class PDFGenDAOPostgres:
 
     def get_title(self, id_meeting, id_user):
         return self.__execute("select "
-                                  "owner.name, "
-                                  "owner.patronymic,"
-                                  "owner.surname,"
-                                  "building.address,"
-                                  "building.street,"
-                                  "building.street_number,"
-                                  "building.block,"
-                                  "building.block_type, "
-                                  "property_rights.regnumber,"
-                                  "property_rights.share_numerator,"
-                                  "property_rights.share_denominator,"
-                                  "property_rights.regdate,"
-                                  "premise.area_rosreestr "
+                              "owner.name, "
+                              "owner.patronymic,"
+                              "owner.surname,"
+                              "building.address,"
+                              "building.street,"
+                              "building.street_number,"
+                              "building.block,"
+                              "building.block_type, "
+                              "property_rights.regnumber,"
+                              "property_rights.share_numerator,"
+                              "property_rights.share_denominator,"
+                              "property_rights.regdate,"
+                              "premise.area_rosreestr "
 
                               "from "
-                                  "meeting,"
-                                  "building,"
-                                  "premise,"
-                                  "property_rights,"
-                                  "owner,"
-                                  "\"User\""
+                              "meeting,"
+                              "building,"
+                              "premise,"
+                              "property_rights,"
+                              "owner,"
+                              "\"User\""
                               "where "
-                                  "meeting.id_meeting = " + str(id_meeting) + " AND "
-                                  "\"User\".id_user = " + str(id_user) +" AND "
-                                  "\"User\".id_owner = owner.id_owner AND "
-                                  "meeting.id_building = building.id_building AND "
-                                  "building.id_building = premise.id_building AND "
-                                  "premise.id_premise = property_rights.id_premise AND "
-                                  "property_rights.id_owner = owner.id_owner")
-
+                              "meeting.id_meeting = " + str(id_meeting) + " AND "
+                                                                          "\"User\".id_user = " + str(id_user) + " AND "
+                                                                                                                 "\"User\".id_owner = owner.id_owner AND "
+                                                                                                                 "meeting.id_building = building.id_building AND "
+                                                                                                                 "building.id_building = premise.id_building AND "
+                                                                                                                 "premise.id_premise = property_rights.id_premise AND "
+                                                                                                                 "property_rights.id_owner = owner.id_owner")
 
     def __execute(self, query):
         cursor = self.conn.cursor()
