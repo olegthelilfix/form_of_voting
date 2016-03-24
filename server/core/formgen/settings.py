e50c19abb15b4210610c1460b0f86991f5962d85
Rentert <smith5989@gmail.com>
2016-03-24 07:18:11 +0300

    add settings

---
+++ server/core/formgen/settings.py
@@ -0,0 +1,6 @@
+DIR_TO_PROJECT = "/home/legionem/pychar/form_of_voting_gen/server/core/formgen/"
+DB_USER = "postgres"
+DB_PASSWORD = "smith620695"
+DB_NAME = "form"
+DB_HOST = "localhost"
+DB_PORT = 5432
